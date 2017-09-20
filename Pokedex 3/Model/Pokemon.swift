98a55b595b7a9a1d494fe80efdbbc138ae11f71f
Gabriel T <gabriel.tapuc@gmail.com>
2017-09-20 10:11:53 -0400

    Added all 718 pokemons in the collection view.

---
+++ Pokedex 3/Model/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  Pokedex 3
+//
+//  Created by Gabriel T on 2017-09-19.
+//  Copyright © 2017 Gabriel T. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
---
+++ Pokedex 3/View/PokeCell.swift
@@ -0,0 +1,32 @@
+//
+//  PokeCellCollectionViewCell.swift
+//  Pokedex 3
+//
+//  Created by Gabriel T on 2017-09-19.
+//  Copyright © 2017 Gabriel T. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+}
